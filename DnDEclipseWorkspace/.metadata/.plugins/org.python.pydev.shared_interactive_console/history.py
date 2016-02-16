016a4ad85046c21bc22cff0465986b150efce389
Luke Lohden <lukasl@mac.com>
2016-02-15 19:48:39 -0800

    Created Eclipse Workspace to begin development

---
+++ DnDEclipseWorkspace/.metadata/.plugins/org.python.pydev.shared_interactive_console/history.py
@@ -0,0 +1,46 @@
+import sys; print('%s %s' % (sys.executable or sys.platform, sys.version))
+{0:,} .format(2**100)
+{0:,} .format(2**100)
+{0:,} .format(2**100)
+{0:,} .format(2**100)
+{0:,} .format(2**100)
+{0:,} .format(2**100)
+print('hello)
+print('hello')
+import sys; print('%s %s' % (sys.executable or sys.platform, sys.version))
+from history import History
+x = History()
+x.a = 1
+import sys; print('%s %s' % (sys.executable or sys.platform, sys.version))
+from history import History
+x = History()
+x.history_dict
+import sys; print('%s %s' % (sys.executable or sys.platform, sys.version))
+from bag import Bag
+b2 = Bag(['b','d','a','b','d','c','d'])
+sorted(b2)
+[i for i in b2]
+b2.bag
+iter
+iter(b2)
+Bag.__iter__(b2)
+import sys; print('%s %s' % (sys.executable or sys.platform, sys.version))
+from bag import Bag
+b2 = Bag(['a','b','b','c','d','d','d'])
+iter(b2)
+sorted(b)
+sorted(b2)
+import sys; print('%s %s' % (sys.executable or sys.platform, sys.version))
+b2 = Bag(['a','b','b','c','d','d','d'])
+from bag import Bag
+b2 = Bag(['a','b','b','c','d','d','d'])
+b3 = Bag(['a','b','c','d','d','d'])
+b2 == b3
+b2 != b3
+b2 == 1
+b2 != 1
+import sys; print('%s %s' % (sys.executable or sys.platform, sys.version))
+print(code_metric('cmtest.py'))
+from q5solution import code_metric
+print(code_metric('cmtest.py'))
+print(code_metric('cmtest.py'))
